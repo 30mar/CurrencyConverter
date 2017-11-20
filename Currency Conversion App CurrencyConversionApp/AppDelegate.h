e7fba0eb3113ae502b3e76d51b061a2f67f9f98c
30mar <omarabdelaziz125@gmail.com>
2017-11-21 01:21:37 +0200

    Initial Commit

---
+++ Currency Conversion App CurrencyConversionApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Currency Conversion App CurrencyConversionApp
+//
+//  Created by Macbook on 11/21/17.
+//  Copyright © 2017 Macbook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Currency Conversion App CurrencyConversionApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Currency Conversion App CurrencyConversionApp
+//
+//  Created by Macbook on 11/21/17.
+//  Copyright © 2017 Macbook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
