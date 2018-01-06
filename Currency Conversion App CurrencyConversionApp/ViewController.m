c1f9a07f6ad36b37149fbe32a6566a2cdc513693
30mar <omarabdelaziz125@gmail.com>
2018-01-06 06:14:10 +0200

    final

--- Currency Conversion App CurrencyConversionApp/ViewController.m
+++ Currency Conversion App CurrencyConversionApp/ViewController.m
@@ -7,21 +7,51 @@
 //
 
 #import "ViewController.h"
+#import "CRCurrencyRequest.h"
+#import "CRCurrencyResults.h"
+@interface ViewController () <CRCurrencyRequestDelegate>
 
-@interface ViewController ()
+@property (nonatomic) CRCurrencyRequest *req;
+// THE OUTLET FOR THE LABELS
+@property (weak, nonatomic) IBOutlet UITextField *inputField;
+@property (weak, nonatomic) IBOutlet UIButton *convertButton;
+@property (weak, nonatomic) IBOutlet UILabel *euroRate;
+@property (weak, nonatomic) IBOutlet UILabel *yenRate;
+@property (weak, nonatomic) IBOutlet UILabel *poundRate;
 
 @end
 
 @implementation ViewController
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+// THE BUTTON METHOD
+- (IBAction)ButtonTapped:(id)sender {
+    self.convertButton.enabled = false;
+    self.req = [[CRCurrencyRequest alloc]init];
+    self.req.delegate = self;
+    [self.req start];
+    
+    
+    
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)currencyRequest:(CRCurrencyRequest *)req
+    retrievedCurrencies:(CRCurrencyResults *)currencies{
+    self.convertButton.enabled = true;
+    double inputValue = [self.inputField.text floatValue];
+    double euroValue = inputValue * currencies.EUR;
+    double yenValue = inputValue * currencies.JPY;
+    double englishPoundValue = inputValue * currencies.GBP;
+    
+    NSString *euro = [NSString stringWithFormat:@"%.2f",euroValue];
+    NSString *yen = [NSString stringWithFormat:@"%.2f",yenValue];
+    NSString *pound = [NSString stringWithFormat:@"%.2f",englishPoundValue];
+
+    
+    self.euroRate.text = euro;
+    self.yenRate.text = yen;
+    self.poundRate.text = pound;
 }
+
+
 
 @end
